1a738516cfb657aa27fcc9394151e25d98cb9185
dscotese <dscotese@litmocracy.com>
2013-04-02 17:46:13 -0700

    Added a couple questions and answers.

--- faq.php
+++ faq.php
@@ -22,6 +22,34 @@
     hopefully many of them will like your answer.  Once the contest is decided,
     some bitcoin will be deposited into the address you entered as a reward for
     providing an answer that inspired someone to back it.</li>
+<li><h3>What if I'm willing to risk some bitcoin?</h3>
+    Click one of the prompts and you will see a list of answers to it, each one
+    with a Reserve button next to it.  Reserving an answer guarantees that any
+    bitcoin you send us to back it will be allocated to that answer.  After you hit
+    the Reserve button, you'll see a notice indicating what the last
+    four digits of the amount you send in must be.<br/><br/>
+    Before (and also after) you click teh Reserve button, you'll see a notice
+    about backing the prompt itself.  That's right, you can also invest in the prompt itself,
+    rather than any of its answers,
+    and this is where half of the deducted percentage goes.  The other half is divided between the
+    people who created the responses <em>if they also provided a bitcoin address</em>.
+    That's how this game pays bitcoin for thinking (and being aware of what people will like).
+<li><h3>How do you win?</h3>
+    The system provides a random order to be used if the creator doesn't change it before the deadline
+    (currently 24 hours after the email is sent). &nbsp;With the seven lists from those creators, the
+    system computes a winner and 90% of the bitcoin backing the responses
+    is paid right back out to the backers as follows:<br>
+    <ul style="margin-top: 0; margin-bottom: 0;">
+        <li>The backing for the 7th place response goes to those who backed on the other 6 places,
+        split up proportionally according to how much they put in.</li>
+        <li>Likewise, the backing for 6th place is split up among those who backed the other 5 places.</li>
+        <li>5th place's backing goes to those who backed 4th.</li>
+        <li>4th's goes to 3rd.</li>
+        <li>3rd's goes to 2nd.</li>
+        <li>2nd's goes to 1st.</li>
+        <li>1st's gets their bitcoin back.</li></ul>
+    Those who backed first place thus get a piece of all the backing behind all the other places,
+    plus they receive back what they put behind the winning response.
 <li><h3>If someone earns a very small amount, how do you cover the transaction fee?</h3>
     We won't send bitcoin to an address until we owe that address more than 0.01 BTC.
     The same address must be used to earn more bitcoins in order to avoid spending all
