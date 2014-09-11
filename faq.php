3f5ecacf425544ccdaa27889f7f7cfedca08beda
dscotese <dscotese@localhost>
2014-09-10 17:29:50 -0700

    Debug info now available only on localhost.  DB pw changed.

--- faq.php
+++ faq.php
@@ -5,7 +5,7 @@
 <li><h3>What is a meme race?</h3>
     Here at the MemeRacing site, a meme race is an initial inspirational prompt and all the
     responses that are recorded on the website.  In a random manner, the responses are
-    collected into batches of seven, and the seven creators of those responses are
+    collected into batches of seven, and the seven respondents of those responses are
     given the chance to put them in order from best to worst.  The website takes that
     input and processes it to identify the best of those seven.</li>
 <li><h3>How do I earn bitcoin without spending any?</h3>
@@ -15,7 +15,7 @@
     <li>The next step is for someone (could be you, but then you'd be "spending"
     bitcoin) to back your answer.</li>
     <li>When enough other answers are available and backed,
-    you'll get an email listing your prompt along with six others and a link you can use
+    you'll get an email listing your answer along with six others and a link you can use
     to re-arrange them from best to worst.</li>
     </ul>
     The same email goes to the other 6 people, and
@@ -28,26 +28,26 @@
     bitcoin you send us to back it will be allocated to that answer.  After you hit
     the Reserve button, you'll see a notice indicating what the last
     four digits of the amount you send in must be.<br/><br/>
-    Before (and also after) you click teh Reserve button, you'll see a notice
+    Before (and also after) you click the Reserve button, you'll see a notice
     about backing the prompt itself.  That's right, you can also invest in the prompt itself,
     rather than any of its answers,
     and this is where half of the deducted percentage goes.  The other half is divided between the
-    people who created the responses <em>if they also provided a bitcoin address</em>.
+    people who created the answers <em>if they also provided a bitcoin address</em>.
     That's how this game pays bitcoin for thinking (and being aware of what people will like).
 <li><h3>How do you win?</h3>
-    The system provides a random order to be used if the creator doesn't change it before the deadline
-    (currently 24 hours after the email is sent). &nbsp;With the seven lists from those creators, the
+    Each respondent that has an answer in a contest gets a chance to order all the answers.  The system provides
+    an individualized random order for anyone who doesn't change it before the deadline
+    (currently 24 hours after the email is sent). &nbsp;With the seven lists from those seven respondents, the
     system computes a winner and 90% of the bitcoin backing the responses
     is paid right back out to the backers as follows:<br>
     <ul style="margin-top: 0; margin-bottom: 0;">
         <li>The backing for the 7th place response goes to those who backed on the other 6 places,
         split up proportionally according to how much they put in.</li>
         <li>Likewise, the backing for 6th place is split up among those who backed the other 5 places.</li>
-        <li>5th place's backing goes to those who backed 4th.</li>
-        <li>4th's goes to 3rd.</li>
-        <li>3rd's goes to 2nd.</li>
-        <li>2nd's goes to 1st.</li>
-        <li>1st's gets their bitcoin back.</li></ul>
+        <li>5th place's backing goes to those who backed the better four.</li>
+        <li>4th's goes to backers of the better three.</li>
+        <li>3rd's to the backers of the better two.</li>
+        <li>2nd's and 1st's all goes to the backers of 1st place.</li></ul>
     Those who backed first place thus get a piece of all the backing behind all the other places,
     plus they receive back what they put behind the winning response.
 <li><h3>If someone earns a very small amount, how do you cover the transaction fee?</h3>
@@ -58,9 +58,9 @@
     There is no minimum backing at this time, but we don't issue payouts for amounts that are
     less than 0.01 BTC.
     </li>
-<li><h3>Won't the creators of entries just put their own creation in first place?</h3>
+<li><h3>Won't the respondents just put their own answer in first place?</h3>
     We assume that most
-    response creators will put their own response first, so that doesn't really help
+    respondents will put their own response first, so that doesn't really help
     sort out the best meme.  However, they will also be sorting the other six memes, which
     does help.</li>
 </ol>
