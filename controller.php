2a360706b44b016b86dee5654ec2272a9c9196a7
dscotese <dscotese@localhost>
2014-11-10 19:19:12 -0800

    Changed amount of time a slot will be left assigned to a whole hour. First attempt to commit said authorization failed. Looks like a bug in Hg - it wouldn't let me try again, so this change is innocuous.

--- controller.php
+++ controller.php
@@ -3,10 +3,10 @@
 // Reservation is how long we leave a slot open before reusing it.
 // The amount of time the user is given is hardcoded to 15 minutes.
 // The extra 45 minutes is because blockchain.info has to call the callback
-//  at least once to lock in the backing, and we want to give it enough time.
+//  to lock in the backing, and we want to give it enough time.
 // A whole hour might be overkill since the target is 10 minutes, but
 // there is little reason not to be overly cautious here.
-// --------------------------------------------------------------------------
+// ------------------------------------------------------------------------
 $reservation = "1 hour";
 
 include("process.php");
